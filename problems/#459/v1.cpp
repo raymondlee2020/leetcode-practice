d6a3137764ca9cf562c3a80c993a951be22c46e2
Raymond Lee <raymond102033@gmail.com>
2023-08-21 14:28:14 +0800

    feat: #459

---
+++ problems/#459/v1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int i = 0, j = 1, n = s.size();
+        vector<int> dp(n + 1, 0);
+        while (j < n) {
+            if (s[i] == s[j]) {
+                dp[++j] = ++i;
+            } else if (i == 0) {
+                ++j;
+            } else {
+                i = dp[i];
+            }
+        }
+        return dp[n] && (dp[n] % (n - dp[n])) == 0;
+    }
+};
