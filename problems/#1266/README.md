e50a0bac61ef7e0176a7189605696dd6700c9dc7
Raymond Lee <raymond102033@gmail.com>
2023-12-03 11:32:21 +0800

    feat: #1266

---
+++ problems/#1266/README.md
@@ -0,0 +1,9 @@
+# Problem 1266 - Minimum Time Visiting All Points
+
+## V1
+
+Time Complexity: O(n)
+
+Space Complexity: O(1)
+
+- Diagonal move is allowable, so only need "longer side" seconds to move between two points.
---
+++ problems/#1266/v1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size(), ret = 0;
+        for (int i = 1; i < n; ++i) {
+            ret += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
+        }
+        return ret;
+    }
+};
